13d81f88d497b8a0f011eb83940f47d685583182
cl676767 <chenggao.li.cl@gmail.com>
2025-10-11 22:00:21 -0400

    Updated based on feedback

--- src/humanoid_vision/training_pipeline.py
+++ src/humanoid_vision/training_pipeline.py
@@ -1,19 +1,31 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader, random_split, TensorDataset
+from torch.utils.data import DataLoader, random_split
+from torch.utils.data import TensorDataset 
 
 class TrainingPipeline ():
-    def __init__(self,model,data,batch_size=32,lr =0.01,epochs=100,device = None):
-        self.device = device or torch.device("cuda" if torch.cuda_is_available() else "cpu")
+    def __init__(self,model,data,batch_size=32,lr =0.01,epochs=100, optimizer = optim.Adam, criterion = nn.BCELoss, device = None):
+        '''
+        Initialize the Training Pipeline
+        :param model: PASS IN MODEL CLASS
+        :param data: pass into data in tensor form
+        :param batch_size: batch size for training
+        :param lr: learning rate
+        :param epochs: Number of training epochs
+        :param device: The device model and data will be passed into(ie cpu or cuda)
+        :param optimizer: PASS IN THE OPTIMIZER CLASS
+        :param criterion: PASS IN LOSS FUNCTION CLASS
+        '''
+        self.device = device or torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model = model.to(self.device)
         self.batch_size = batch_size
-        self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = optim.Adam(self.model.parameters(),lr = lr)
+        self.criterion = criterion()
+        self.optimizer = optimizer(self.model.parameters(),lr = lr)
         self.epochs = epochs
 
         #train test split(0.8:0.2)
-        train_size = int(len(data))
+        train_size = int(0.8* len(data))
         val_size = len(data) - train_size
         train_data , val_data = random_split(data,[train_size,val_size])
 
@@ -23,13 +35,17 @@ class TrainingPipeline ():
 
 
     def train_one_epoch(self):
+        '''
+        Runs one training loop through the model using training data
+        :return: average loss for the batch
+        '''
         self.model.train()
         total_loss = 0
         #loops through each batch
         for xb,yb in self.train_loader:
             xb , yb = xb.to(self.device) , yb.to(self.device)
-            preds = self.model.predict(xb)
-            loss = self.criterion(yb,preds)
+            preds = self.model(xb)
+            loss = self.criterion(preds,yb)
             total_loss += loss.item()
 
             self.optimizer.zero_grad()
@@ -40,20 +56,27 @@ class TrainingPipeline ():
             
 
     def validate(self):
+        '''
+        Evaluates the current model using testing data
+        :return: average loss for the batch
+        '''
         self.model.eval()
         total_loss = 0
-        with torch.inference_mode:
+        with torch.inference_mode():
             #loops through each batch
-            for xb,yb in self.val_data:
+            for xb,yb in self.val_loader:
                 xb , yb = xb.to(self.device) , yb.to(self.device)
-                preds = self.model.predict(xb)
-                loss = self.criterion(yb,preds)
+                preds = self.model(xb)
+                loss = self.criterion(preds,yb)
                 total_loss += loss.item()
 
                 
         return total_loss / len(self.val_loader) #returns average loss for the batch
 
     def training(self):
+        '''
+        Loop where all the training actually happens
+        '''
         for epoch in range(self.epochs):
             train_loss = self.train_one_epoch()
             val_loss = self.validate()
@@ -61,6 +84,25 @@ class TrainingPipeline ():
                 print(f"Train loss: {train_loss}    Validation loss:{val_loss}")
 
     def save(self,path):
+        '''
+        Saves the model to a given path
+        :param path: path where model is saved
+        '''
         torch.save(self.model.state_dict(),path)
 
-        
+#test code
+
+testModel = nn.Sequential(
+    nn.Linear(3,1),
+    nn.Sigmoid()
+)
+
+#size of batchsize then 10
+X = torch.randn(100,3)
+print(X)
+y = torch.randint(0, 2, (100, 1)).float()
+
+testData = TensorDataset(X,y)
+
+pipeline = TrainingPipeline(model = testModel,batch_size = 10,data = testData, device = "cpu")
+pipeline.training()
